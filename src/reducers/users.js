35e3990e58ccb1cbd05fe170712bc9639458e24d
Erickson Alves <erickson.higor@gmail.com>
2021-10-25 21:06:33 -0400

    Handle question actions in users
    Signed-off-by: Erickson Alves <erickson.higor@gmail.com>

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,9 +1,26 @@
 import { RECEIVE_DATA } from '../actions/shared';
+import { ADD_ANSWER, ADD_QUESTION } from '../actions/question';
 
 export default function users(state = [], action) {
   switch (action.type) {
     case RECEIVE_DATA:
       return action.users;
+    case ADD_ANSWER:
+      const { questionId, userId, answer } = action;
+
+      let user = state.find((u) => u.id === userId);
+
+      user.answers[questionId] = answer;
+
+      return state.filter((u) => u.id !== user.id).concat([user]);
+    case ADD_QUESTION:
+      const question = action.question;
+
+      let author = state.find((u) => u.id === question.author);
+
+      author.questions = author.questions.concat([question.id]);
+
+      return state.filter((u) => u.id !== author.id).concat([author]);
     default:
       return state;
   }
